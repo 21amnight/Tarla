80f3c999a3d6596b791a94e6410d4cc1168d7557
21amnight <aitenirasanov@gmail.com>
2021-12-03 16:39:35 +0600

    first

---
+++ src/JS/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        // autoplayTimeout:2,
+        autoplaySpeed: 4,
+        autoplayHoverPause: true,
+
+    });
+});
+$(document).ready(function(){
+    $(".carousel1").owlCarousel({
+        items: 3,
+        loop:true,
+         autoplay:true,
+        autoplaySpeed: 4,
+         // autoplayTimeout: 1,
+         autoplayHoverPause:true,
+    });
+});
